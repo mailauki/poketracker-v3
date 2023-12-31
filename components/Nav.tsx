90fcb9aee9a9b15219e29021019ecd24d91bce0f
Julie Evans <juliemevans17@gmail.com>
2023-12-31 17:35:25 -0600

    Update nav

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,29 +1,12 @@
 'use client'
 
-import { Button, Divider, Tab, Tabs, useTheme } from "@mui/material"
+import { Box, Button, Divider, List, ListItemButton, ListItemIcon, ListItemText, Tab, Tabs, useTheme } from "@mui/material"
 import Link from "next/link"
 import { ReactElement, useEffect, useState } from "react"
 import { Game } from "@/utils/types"
 import { usePathname } from "next/navigation"
 import { Person, Settings, CatchingPokemon, Login, Logout } from "@mui/icons-material"
 import { adjustName } from "@/utils/helper"
-import NavTab from "./NavTab"
-
-function samePageLinkNavigation(
-  event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
-) {
-  if (
-    event.defaultPrevented ||
-    event.button !== 0 || // ignore everything but left-click
-    event.metaKey ||
-    event.ctrlKey ||
-    event.altKey ||
-    event.shiftKey
-  ) {
-    return false;
-  }
-  return true;
-}
 
 interface LinkTabProps {
   label?: string;
@@ -39,18 +22,8 @@ const LinkTab = function(props: LinkTabProps) {
   return (
     <Tab
       component={props.href == '/signout' ? Button : Link}
-      // onClick={(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
-      //   // Routing libraries handle this, you can remove the onClick handle when using them.
-      //   if (samePageLinkNavigation(event)) {
-      //     event.preventDefault();
-      //   }
-      // }}
       {...props}
-      // href={props.href == '/signout' ? '' : props.href}
       type={props.href == '/signout' ? 'submit' : ''}
-      // value={props.href}
-      // label={props.label}
-      // icon={props.icon}
       iconPosition='start'
       sx={{
         width: '100%',
@@ -80,32 +53,33 @@ export default function Nav() {
     setValue(pathname)
   }, [pathname])
 
-  const tabs = [
-    {
-      href: '/',
-      label: 'home',
-      icon: <CatchingPokemon />
-    }, {
-      href: '/login',
-      label: 'login',
-      icon: <Login />
-    }, {
-      href: '/profile',
-      label: 'profile',
-      icon: <Person />
-    }, {
-      href: '/account',
-      label: 'account settings',
-      icon: <Settings />
-    }
-  ]
+  const LinkButton = function(props: LinkTabProps) {
+    return (
+      <ListItemButton
+        selected={value == props.href}
+        component={props.href == '/signout' ? ListItemButton : Link}
+        href={props.href}
+        type={props.href == '/signout' ? 'submit' : ''}
+        sx={{
+          textTransform: 'uppercase',
+          borderRight: '4px solid',
+          borderColor: value == props.href ? theme.palette.primary.main : 'transparent'
+        }}
+      >
+        <ListItemIcon>
+          {props.icon}
+        </ListItemIcon>
+        <ListItemText primary={props.label} />
+      </ListItemButton>
+    )
+  }
 
   return (
     <>
-      <Tabs
+      {/* <Tabs
         value={value}
-        aria-label="nav tabs"
-        role="navigation"
+        // aria-label='nav tabs'
+        // role='navigation'
         orientation='vertical'
         variant='scrollable'
         sx={{ width: '100%' }}
@@ -116,6 +90,15 @@ export default function Nav() {
           value='/'
           icon={<CatchingPokemon />}
         />
+        <LinkTab
+          label='login'
+          href='/login'
+          value='/login'
+          icon={<Login />}
+        />
+
+        <Divider />
+
         <LinkTab
           label='profile'
           href='/profile'
@@ -147,25 +130,56 @@ export default function Nav() {
             sx={{ alignItems: 'flex-start', '&:hover': { bgcolor: theme.palette.action.hover } }}
           />
         )}
-      </Tabs>
-      {/* <List sx={{ mb: 4 }}>
-        <ListItemButton
-          component={Link}
-          href="/"
-        >
-          <ListItemText primary="home" />
-        </ListItemButton>
-        <Divider />
-        {games.map(game => 
-          <ListItemButton
-            key={game.name}
-            component={Link}
-            href={`/game/${game.name}`}
-          >
-            <ListItemText primary={game.name} />
-          </ListItemButton>
-        )}
-      </List> */}
+      </Tabs> */}
+      <Box sx={{ width: '100%', bgcolor: 'background.paper', overflowY: 'scroll', pb: 3 }}>
+        <List component='nav'>
+          <LinkButton
+            href='/'
+            label='home'
+            icon={<CatchingPokemon />}
+          />
+          <LinkButton
+            label='login'
+            href='/login'
+            value='/login'
+            icon={<Login />}
+          />
+  
+          <Divider />
+  
+          <LinkButton
+            label='profile'
+            href='/profile'
+            value='/profile'
+            icon={<Person />}
+          />
+          <LinkButton
+            label='account settings'
+            href='/account'
+            value='/account'
+            icon={<Settings />}
+          />
+          <form action='/auth/signout'>
+            <LinkButton
+              href='/signout'
+              label='signout'
+              value='/signout'
+              icon={<Logout />}
+            />
+          </form>
+
+          <Divider />
+
+          {games.map((game) => 
+            <LinkButton
+              key={game.name}
+              href={`/game/${game.name}`}
+              value={`/game/${game.name}`}
+              label={`${adjustName(game.name)}`}
+            />
+          )}
+        </List>
+      </Box>
     </>
   )
 }
