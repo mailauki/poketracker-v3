b89d87b5ab241157e37b82be0264f7fd1f73a518
Julie Evans <juliemevans17@gmail.com>
2024-01-05 22:05:30 -0600

    Update pokemon card

--- components/pokemon/PokeCard.tsx
+++ components/pokemon/PokeCard.tsx
@@ -1,4 +1,4 @@
-import { Box, Card, CardHeader, CardMedia, IconButton, Paper, Stack, Typography } from "@mui/material"
+import { Box, Card, CardActionArea, CardActions, CardHeader, CardMedia, IconButton, Paper, Stack, Typography } from "@mui/material"
 import { padZero } from "@/utils/helper"
 import { useEffect, useState } from "react"
 import { PokeProps, Sprites } from "@/utils/types"
@@ -8,85 +8,106 @@ import { More, MoreVert } from "@mui/icons-material"
 
 export default function PokeCard({ pokemon }: PokeProps) {
   const [sprites, setSprites] = useState<Sprites | undefined>(undefined)
+  const [loading, setLoading] = useState(false)
+  const [captured, setCaptured] = useState(false)
+  console.log(pokemon)
 
   useEffect(() => {
     fetch(pokemon.pokemon_species.url)
     .then((res) => res.json())
     .then((data) => {
+      setLoading(true)
+
       // console.log(data.evolution_chain.url)
       fetch(data.varieties[0].pokemon.url)
       .then((res) => res.json())
-      .then((data) => setSprites(data.sprites))
+      .then((data) => {
+        setSprites(data.sprites)
+
+        setLoading(false)
+      })
     })
   }, [pokemon.pokemon_species.url])
 
   return (
-    <Card sx={{ width: 150, height: 150, position: 'relative' }}>
-      <IconButton
-        aria-label="captured"
-        color='inherit'
-        sx={{
-          color: 'GrayText',
-          position: 'absolute',
-          bottom: 2,
-          right: 2,
-          zIndex: 2
-        }}
-      >
-        <MoreVert />
-      </IconButton>
-
-      <Box
-        sx={{
-          position: 'absolute',
-          width: '100%',
-          height: '100%',
-          zIndex: 0
-        }}
+    <Card
+      sx={{ width: 150, height: 150, position: 'relative' }}
+      // elevation={captured ? 0 : 1}
+      variant={captured ? "outlined" : "elevation"}
+    >
+      <CardActionArea
+        sx={{ width: '100%', height: '100%' }}
+        onClick={() => setCaptured(!captured)}
       >
         <Box
           sx={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
+            position: 'absolute',
             width: '100%',
-            height: '100%'
+            height: '100%',
+            zIndex: 0
           }}
         >
-          {sprites ? (
-            <CardMedia
-              component="img"
-              sx={{
-                width: 100,
-                height: 100,
-                mx: 'auto'
-              }}
-              image={sprites?.front_default}
-              alt={pokemon.pokemon_species.name}
-            />
-          ) : (
-            <Loading />
-          )}
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              width: '100%',
+              height: '100%'
+            }}
+          >
+            {sprites && !loading ? (
+              <CardMedia
+                component="img"
+                sx={{
+                  width: 100,
+                  height: 100,
+                  mx: 'auto',
+                  filter: captured ? '' : 'grayscale(100%) brightness(0) invert(1) opacity(0.5)'
+                }}
+                image={sprites?.front_default}
+                alt={pokemon.pokemon_species.name}
+              />
+            ) : (
+              <Loading />
+            )}
+          </Box>
         </Box>
-      </Box>
 
-      <Stack
-        justifyContent='space-between'
-        alignItems='center'
-        sx={{
-          width: '100%',
-          height: '100%',
-          p: 1,
-          zIndex: 1
-        }}
-      >
-        <Typography variant='overline'>
-          {pokemon?.pokemon_species.name}
-        </Typography>
-        <Typography color='text.secondary' variant='caption'>
-          {padZero(pokemon?.entry_number)}
-        </Typography>
-      </Stack>
+        <Stack
+          justifyContent='space-between'
+          alignItems='center'
+          sx={{
+            width: '100%',
+            height: '100%',
+            p: 1,
+            zIndex: 1
+          }}
+        >
+          <Typography variant='overline'>
+            {pokemon?.pokemon_species.name}
+          </Typography>
+          <Typography color='text.secondary' variant='caption'>
+            {padZero(pokemon?.entry_number)}
+          </Typography>
+        </Stack>
+      </CardActionArea>
+      <CardActions>
+        <IconButton
+          aria-label="more info"
+          color='inherit'
+          sx={{
+            color: 'GrayText',
+            position: 'absolute',
+            bottom: 2,
+            right: 2,
+            zIndex: 3
+          }}
+          onClick={() => console.log('more info')}
+        >
+          <MoreVert />
+        </IconButton>
+      </CardActions>
     </Card>
   )
 }
